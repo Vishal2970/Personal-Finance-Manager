2ad581dc4bd35c96eb0ea4123eeca49d4b0cb26c
Trainings <Trainings@datanova.co.in>
2024-07-22 14:12:31 +0530

    Fix css problem

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -42,7 +42,7 @@ const Register = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh",
+        blockSize: "100vh",
       }}
     >
       <div
@@ -50,7 +50,7 @@ const Register = () => {
         style={{
           boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
           transition: "0.3s",
-          width: "300px",
+          inlineSize: "300px",
           padding: "20px",
         }}
       >
@@ -109,7 +109,7 @@ const Register = () => {
               Register
             </button>
           </form>
-          <div style={{ marginTop: "10px" }}>
+          <div style={{ insetBlockStart: "10px" }}>
             <p> Already Registered ?</p>
             <Link to="/" className="btn btn-secondary">
               Sign In
@@ -122,89 +122,3 @@ const Register = () => {
 };
 
 export default Register;
-
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
-
-// const Login = () => {
-//   const [login, setLogin] = useState({
-//     email: "",
-//     password: "",
-//   });
-
-//   const navigate = useNavigate();
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     alert("Login successfull")
-//     navigate("/home")
-//   };
-
-//   const handleChange = (e) => {
-//     let name = e.target.name;
-//     let value = e.target.value;
-//     setLogin({ ...login, [name]: value });
-//   };
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         height: "100vh",
-//       }}
-//     >
-//       <div
-//         className="card"
-//         style={{
-//           boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
-//           transition: "0.3s",
-//           width: "300px",
-//           padding: "20px",
-//         }}
-//       >
-//         <div className="card-body">
-//           <h5 className="card-title">Login</h5>
-//           <form onSubmit={handleSubmit}>
-//             <div className="mb-3">
-//               <label htmlFor="email" className="form-label">
-//                 Email address
-//               </label>
-//               <input
-//                 type="email"
-//                 className="form-control"
-//                 name="email"
-//                 value={login.email}
-//                 onChange={handleChange}
-//               />
-//             </div>
-//             <div className="mb-3">
-//               <label htmlFor="password" className="form-label">
-//                 Password
-//               </label>
-//               <input
-//                 type="password"
-//                 className="form-control"
-//                 name="password"
-//                 value={login.password}
-//                 onChange={handleChange}
-//               />
-//             </div>
-//             <button type="submit" className="btn btn-primary">
-//               Sign In
-//             </button>
-//           </form>
-//           <div style={{ marginTop: "10px" }}>
-//             <p>Not signed in?</p>
-//             <Link to="/register" className="btn btn-secondary">
-//               Register
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
